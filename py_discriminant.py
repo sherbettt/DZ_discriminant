59a4acde7c1feafa149bdbc05cded71e794d1682
Kirill K <44464893+sherbettt@users.noreply.github.com>
2025-03-12 19:14:27 +0300

    Update py_discriminant.py

--- py_discriminant.py
+++ py_discriminant.py
@@ -20,6 +20,8 @@ def solve(a,b,c):
         x1 = int((-b - math.sqrt(D)) / (2 * a))
         x2 = int((-b + math.sqrt(D)) / (2 * a))
         return(f' Дискриминант имеет 2 корня: x1 = {x1}, x2 = {x2}')
+    else:
+        return("при D<0 решение нет")
 #    if D < 0:
 #        raise StopIteration
 
@@ -28,6 +30,7 @@ print(discriminant(a,b,c))
 #print(solve(1, 2, 1))
 #print(solve(2, 5, 3))
 #print(solve(1, -1, 3))
+
 
 
 
