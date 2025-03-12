f124a1c03a4b2d5e11a179ee69bd32b9dcbe63f1
Kirill K <44464893+sherbettt@users.noreply.github.com>
2025-03-12 19:00:42 +0300

    Update py_discriminant.py

--- py_discriminant.py
+++ py_discriminant.py
@@ -1,8 +1,8 @@
 import math
 
-a = int(input('Insert a: '))
-b = int(input('Insert b: '))
-c = int(input('Insert c: '))
+#a = int(input('Insert a: '))
+#b = int(input('Insert b: '))
+#c = int(input('Insert c: '))
 
 
 # функция вычисления дискриминанта
@@ -12,7 +12,7 @@ def discriminant(a,b,c):
 
 # квадратное уравнение и основной цикл
 def solve(a,b,c):
-    discriminant()
+    discriminant(a,b,c)
     if D == 0:
         x = -b/2*a
         return(f' Дискриминант имеет 1 корень: x = {x}')
@@ -20,11 +20,15 @@ def solve(a,b,c):
         x1 = int((-b - math.sqrt(D)) / (2 * a))
         x2 = int((-b + math.sqrt(D)) / (2 * a))
         return(f' Дискриминант имеет 2 корня: x1 = {x1}, x2 = {x2}')
-    if D < 0:
-        raise StopIteration
+#    if D < 0:
+#        raise StopIteration
 
-print(discriminant(a,b,c))
-print(solve(a,b,c))
+#print(discriminant(a,b,c))
+#print(solve(a,b,c))
+
+print(solve(1, 2, 1))
+print(solve(2, 5, 3))
+print(solve(1, -1, 3))
 
 
 
